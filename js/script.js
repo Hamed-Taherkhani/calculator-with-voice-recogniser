c88a91e7f4e23c56200545268ef3b5a82a18e6fa
Hamed Taherkhani <hamedtahi7@gmail.com>
2021-10-30 18:37:07 +0330

    Add result preview to home.html and style it,Fix script.js code. also add some code for previwing the result on the determined place;

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const calculatorButtons = document.querySelectorAll("#calculator td section"),
-  displayArea = document.querySelector("#display-area");
+  displayArea = document.querySelector("#display-area"),
+  preview = document.querySelector("#preview-result");
 
 //   Add listener for every buttons and handle them:
 calculatorButtons.forEach((value) => {
@@ -17,20 +18,45 @@ function handelClick() {
   if (value === "CE") {
     // A section of a string from 0 to end - 1 (remove the last character).
     removeLastCharacter(displayAreaValue);
+    // If there are not any operator, removes whole values;
+    if (!hasJSMathOperator(displayAreaValue)) displayArea.textContent = "";
   } else if (value === "=") {
-    operateTheValues(displayAreaValue);
-  } else displayArea.textContent += value;
+    operateTheValues(displayAreaValue, "displayArea");
+  } else if (displayAreaValue.length <= 18) {
+    displayArea.textContent += value;
+  }
+
+  // Show result as a preview result. If exist math operator, preview is shown, otherwise is not shown.
+  if (hasJSMathOperator(displayArea.textContent))
+    operateTheValues(displayArea.textContent, "preview");
+  else preview.textContent = "";
 }
 
 function removeLastCharacter(str) {
-  displayArea.textContent = str.slice(0, str.length - 1);
+  let temp = str.slice(0, str.length - 1);
+  displayArea.textContent = temp;
+  operateTheValues(temp, "preview");
 }
 
-function operateTheValues(values) {
+function operateTheValues(values, whereDisplayed) {
   // ×,÷,− are not a JS math operator. Need to change.
   values = values.replace(/÷/g, "/");
   values = values.replace(/×/g, "*");
   values = values.replace(/−/g, "-");
+  values = values.replace(/%/g, "/100");
   let product = eval(values);
-  displayArea.textContent = product;
+  if (whereDisplayed === "displayArea") {
+    displayArea.textContent = product;
+    // Clear preview section:
+    preview.textContent = "";
+  } else if (whereDisplayed === "preview") preview.textContent = product;
+}
+
+function hasJSMathOperator(str) {
+  return (
+    str.includes("÷") ||
+    str.includes("−") ||
+    str.includes("%") ||
+    str.includes("+")
+  );
 }
